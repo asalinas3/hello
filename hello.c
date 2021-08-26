4ef590e5111a506d636cffde65a88df0602d4160
Abigail Salinas <asalinas3@huskers.unl.edu>
2021-08-26 16:45:44 +0000

    added my major to the output

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Abigail Salinas
+ * Date: 20 08/23/21
+ *
+ * A simple hello world program in C
+ *
+ */
+ #include <stdlib.h>
+ #include <stdio.h>
+
+ int main(int argc, char **argv) {
+
+     printf("Abigail Salinas!\nComputer Science\n");
+    
+
+     return 0;
+ }
